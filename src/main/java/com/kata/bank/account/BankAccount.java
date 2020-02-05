34bdc92156178dac25500b8f3a22c9609dbe98a2
Thierry Wong <wong.thi@gmail.com>
2020-02-06 00:48:10 +0100

    Added deposit history as list so that we know if operations were done on the account - Second test passes

--- src/main/java/com/kata/bank/account/BankAccount.java
+++ src/main/java/com/kata/bank/account/BankAccount.java
@@ -1,22 +1,29 @@
 package com.kata.bank.account;
 
+import com.kata.bank.operation.Deposit;
 import com.kata.bank.statement.StatementPrinter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BankAccount {
 
     private StatementPrinter statementPrinter;
+    private List<Deposit> depositHistory = new ArrayList<>();
 
     public BankAccount(StatementPrinter statementPrinter) {
         this.statementPrinter = statementPrinter;
     }
 
     public void deposit(BigDecimal amountToDeposit) {
-
+        depositHistory.add(new Deposit(amountToDeposit));
     }
 
     public void printStatement() {
+        if (depositHistory.isEmpty()) {
+            return;
+        }
         statementPrinter.print("OPERATION | DATE | AMOUNT | BALANCE");
         statementPrinter.print("DEPOSIT | 29/01/2020 | 20.00 | 20.00");
     }
---
+++ src/main/java/com/kata/bank/operation/Deposit.java
@@ -0,0 +1,11 @@
+package com.kata.bank.operation;
+
+import java.math.BigDecimal;
+
+public class Deposit {
+    private BigDecimal amountToDeposit;
+
+    public Deposit(BigDecimal amountToDeposit) {
+        this.amountToDeposit = amountToDeposit;
+    }
+}
