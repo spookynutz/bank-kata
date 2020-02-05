7d4359841b1d8c2e81410a02a8bbdb612c748f6e
Thierry Wong <wong.thi@gmail.com>
2020-02-05 23:31:52 +0100

    Commit init

---
+++ src/test/java/BankAccountShould.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BankAccountShould {
+
+    @Test
+    public void is_true(){
+        assertThat(true).isTrue();
+    }
+}
