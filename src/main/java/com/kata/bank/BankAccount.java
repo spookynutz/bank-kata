1cca37170f93784ce6c837afc8a519e7f5ec8e01
Thierry Wong <wong.thi@gmail.com>
2020-02-06 00:21:45 +0100

    Added first failing test for the deposit operation

---
+++ src/main/java/com/kata/bank/BankAccount.java
@@ -0,0 +1,13 @@
+package com.kata.bank;
+
+import java.math.BigDecimal;
+
+public class BankAccount {
+    public void deposit(BigDecimal amountToDeposit) {
+
+    }
+
+    public void printStatement() {
+
+    }
+}
--- src/test/java/com/kata/acceptance/BankAccountShould.java
+++ src/test/java/com/kata/acceptance/BankAccountShould.java
@@ -1,13 +1,28 @@
 package com.kata.acceptance;
 
+import com.kata.bank.BankAccount;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BankAccountShould {
 
     @Test
-    public void is_true(){
-        assertThat(true).isTrue();
+    public void allow_client_to_make_a_deposit(){
+        BigDecimal amountToDeposit = BigDecimal.valueOf(20);
+
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(amountToDeposit);
+        bankAccount.printStatement();
+
+        List<String> printedStatements = new ArrayList<>();
+        assertThat(printedStatements).containsExactly(
+                "OPERATION | DATE | AMOUNT | BALANCE",
+                "DEPOSIT | 29/01/2020 | 20.00 | 20.00"
+        );
     }
 }
