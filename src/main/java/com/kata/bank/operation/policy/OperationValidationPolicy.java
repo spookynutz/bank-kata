132fb769484246acc94a8ba47bcf99dcdf540b66
Thierry Wong <wong.thi@gmail.com>
2020-02-06 04:37:20 +0100

    Test passes

--- src/main/java/com/kata/bank/operation/policy/OperationValidationPolicy.java
+++ src/main/java/com/kata/bank/operation/policy/OperationValidationPolicy.java
@@ -2,6 +2,8 @@ package com.kata.bank.operation.policy;
 
 import com.kata.bank.Money;
 
+import java.math.BigDecimal;
+
 public class OperationValidationPolicy {
 
     private OperationValidationPolicy() {
@@ -16,6 +18,14 @@ public class OperationValidationPolicy {
             throw new IllegalArgumentException("Amount to withdraw cannot be null");
         }
 
+        if (amountIsNegative(amountToWithdraw)){
+            throw new IllegalArgumentException("Cannot withdraw negative amount");
+        }
+
         return accountBalance.greaterOrEqualTo(amountToWithdraw);
+    }
+
+    private static boolean amountIsNegative(Money amount) {
+        return new Money(BigDecimal.ZERO).greaterOrEqualTo(amount);
     }
 }
