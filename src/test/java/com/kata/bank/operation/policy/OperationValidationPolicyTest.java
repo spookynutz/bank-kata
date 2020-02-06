f536b35b5f50ca39d1fdcae75beff3b8de8a7165
Thierry Wong <wong.thi@gmail.com>
2020-02-06 04:30:05 +0100

    Adding failing test with null balance given to policy

--- src/test/java/com/kata/bank/operation/policy/OperationValidationPolicyTest.java
+++ src/test/java/com/kata/bank/operation/policy/OperationValidationPolicyTest.java
@@ -12,11 +12,19 @@ public class OperationValidationPolicyTest {
 
     private static final Money AMOUNT_100 = new Money(BigDecimal.valueOf(100));
     private static final Money AMOUNT_50 = new Money(BigDecimal.valueOf(50));
+    private static final Money AMOUNT_NULL = null;
 
     @Test
     public void allow_withdraw_when_account_balance_above_amount_to_withdraw(){
         Money accountBalance = AMOUNT_100;
         Money amountToWithdraw = AMOUNT_50;
         assertThat(isWithdrawAllowed(accountBalance, amountToWithdraw)).isTrue();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throw_exception_when_account_balance_is_null(){
+        Money accountBalance = AMOUNT_NULL;
+        Money amountToWithdraw = AMOUNT_50;
+        assertThat(isWithdrawAllowed(accountBalance, amountToWithdraw));
     }
 }
