760e7ae0be7c4e6f59c28c37364a91ab99641ee8
Thierry Wong <wong.thi@gmail.com>
2020-02-06 04:28:43 +0100

    Adding valid withdraw test

---
+++ src/test/java/com/kata/bank/operation/policy/OperationValidationPolicyTest.java
@@ -0,0 +1,22 @@
+package com.kata.bank.operation.policy;
+
+import com.kata.bank.Money;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static com.kata.bank.operation.policy.OperationValidationPolicy.isWithdrawAllowed;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OperationValidationPolicyTest {
+
+    private static final Money AMOUNT_100 = new Money(BigDecimal.valueOf(100));
+    private static final Money AMOUNT_50 = new Money(BigDecimal.valueOf(50));
+
+    @Test
+    public void allow_withdraw_when_account_balance_above_amount_to_withdraw(){
+        Money accountBalance = AMOUNT_100;
+        Money amountToWithdraw = AMOUNT_50;
+        assertThat(isWithdrawAllowed(accountBalance, amountToWithdraw)).isTrue();
+    }
+}
