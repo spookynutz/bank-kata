d9985e09584a9586c586441aca822a40d30a3cd0
Thierry Wong <wong.thi@gmail.com>
2020-02-06 04:22:29 +0100

    Added empty statement test and refactored

--- src/test/java/com/kata/bank/statement/ConsoleStatementPrinterTest.java
+++ src/test/java/com/kata/bank/statement/ConsoleStatementPrinterTest.java
@@ -1,20 +1,44 @@
 package com.kata.bank.statement;
 
+import com.kata.bank.Money;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConsoleStatementPrinterTest {
 
+    private final StatementPrinter statementPrinter = new ConsoleStatementPrinter();
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream consoleOut = System.out;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(consoleOut);
+    }
+
     @Test
     public void do_nothing_with_null_statement(){
-        StatementPrinter statementPrinter = new ConsoleStatementPrinter();
         statementPrinter.print(null);
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
         assertThat(output.toString()).isEmpty();
     }
+
+    @Test
+    public void print_nothing_when_statement_has_no_statement_lines(){
+        statementPrinter.print(new Statement(new ArrayList<>(), new Money(BigDecimal.ZERO)));
+        assertThat(output.toString()).isEmpty();
+    }
+
+
 }
